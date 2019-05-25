74f7659eed193e1776f634ec2d88f35bfc4faf23
Ruben Lazarin <rubenlazarin03@gmail.com>
2019-05-25 12:23:21 -0500

    Login, register, and logout work. No single user display.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request, redirect, session, flash
 from flask_sqlalchemy import SQLAlchemy
+from hashutils import make_pw_hash, check_pw_hash
 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:bloging@localhost:8889/blogz'
-
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zP3B'
 
 
 class Blog(db.Model):
@@ -16,10 +16,70 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(15))
     body = db.Column(db.String(300))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
+
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    pw_hash = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, email, password):
+        self.email = email
+        self.pw_hash = make_pw_hash(password)
+
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect('/login')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and check_pw_hash(password, user.pw_hash):
+            session['email'] = email
+            flash("Logged in")
+
+            return redirect('/')
+
+        else:
+            flash("Password incorrect or User does not exist.", "error")
+
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['email'] = email
+            return redirect('/')
+        else:
+            return '<h1>Duplicate user</h1>'
+    
+    return render_template('register.html')
+
 
 
 @app.route('/')
@@ -43,6 +103,7 @@ def all_blogs():
 @app.route('/postblog', methods=['POST', 'GET'])
 def post_blog():
 
+    owner = User.query.filter_by(email=session['email']).first()
 
     if request.method == 'POST':
         blog_title = request.form['blog-title']
@@ -51,7 +112,7 @@ def post_blog():
         blog_body_error = ""
 
         if blog_title != "" and blog_body != "":
-            new_blog = Blog(blog_title, blog_body)
+            new_blog = Blog(blog_title, blog_body, owner)
             db.session.add(new_blog)
             db.session.commit()
             return redirect('/blog?id={0}'.format(new_blog.id))
@@ -78,6 +139,10 @@ def post_blog():
     return render_template('postblog.html', title="Post-Blog")
 
 
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect('/')
 
 
 
